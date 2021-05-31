25a7b139a62759145ee6c987df700cda678424e2
giovaterry <74530756+giovaterry@users.noreply.github.com>
2021-05-31 10:41:53 +0200

    javascipt
    Update

---
+++ jquery.js
@@ -0,0 +1,7 @@
+function myFunction()
+{
+
+document.getElementById("kob").style.visibility = 'hidden';
+document.getElementById("antal-varer").style.visibility = 'visible';
+var change = document.getElementById("fire") === document.getElementById("water");
+}
