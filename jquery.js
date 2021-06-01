a7a961232f89b4808576503af350be9749b8babd
giovaterry <74530756+giovaterry@users.noreply.github.com>
2021-06-01 11:09:33 +0200

    pop up update
    update

--- jquery.js
+++ jquery.js
@@ -52,6 +52,71 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}}
+
+  
+
+
+
+
+
+
+
+
+  // Get the modal
+var modal = document.getElementById("myModalKiwi");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnKiwi");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-kiwi")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+ 
+
+    // Get the modal
+var modal = document.getElementById("myModalBanan");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnBanan");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-banan")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  
+
+}
+}
+}
+}
 
 
